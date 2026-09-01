9a136b9ec6d53049a6fee2060fafb291926850d7
Hyj <nn50241125huangd@163.com>
2026-09-01 11:05:13 +0800

    Create Charcter and Ememy

---
+++ Source/Aura/Public/Character/AuraEmemy.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraEmemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraEmemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
